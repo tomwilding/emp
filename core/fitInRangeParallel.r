02fca5b8acb3902b260566c164f1e953c6d7a7d9
tomwilding <tomwilding72@gmail.com>
2014-02-14 00:21:36 +0000

    Refactored out RSq code

--- core/fitInRangeParallel.r
+++ core/fitInRangeParallel.r
@@ -1,14 +1,14 @@
 fitInRangeParallel <- function(optimSIRMulti, i, times, data, initConds, initParams, ts, k, range, actualFit, plotConfig) {
 	require(doMC)
-	# source('breakTime.r')
+	require(assertive)
 
-	# cl <- makeCluster(3)
-	# registerDoParallel(cl)
-	# clusterExport(cl, c("sseSIRMulti", "lsoda"))
-	# numCores <- 15
-	# registerDoMC(numCores)
-	# print(length(range))
-	registerDoMC(length(range))
+	# Ensure length of range is greater than 0
+	assert_all_are_true(length(range) > 0)
+
+	# Register multi core backend with n cores
+	n <- length(range)
+	registerDoMC(n)
+
 	# Truncate data set
 	truncTimes <- times[1:i]
 	truncData <- data[1:i]
@@ -17,17 +17,14 @@ fitInRangeParallel <- function(optimSIRMulti, i, times, data, initConds, initPar
 	timeStep <- 0.05
 	fineTimes <- breakTime(times, timeStep);
 
-	rSquareMulti <- 0
-	maxRSquare <- 0
+	# Set optimParams to initParams to use if optimisation fails
 	optimParams <- initParams
-
 	# Range of feasible t0 values to explore referenced from offset data
 	# t0 <- proc.time()
 	allOptimParams <- foreach (t=range, .combine="rbind") %dopar% {
 		# time value t0 referenced from offset data
 		tsExplore <- c(ts[1:k-1],t)
-		# Find optimal beta and gamma by optimising them to minimise the least square function
-		# OptimSIRMulti passed in from call to setSolver
+		# Find optimal beta and gamma by optimising them to minimise the least square function - OptimSIRMulti passed in from call to setSolver
 		tryCatch({
 			optimParams <- optimSIRMulti(truncTimes, truncData, initConds, initParams, tsExplore, k)
 		}, warning = function(w) {
@@ -35,45 +32,28 @@ fitInRangeParallel <- function(optimSIRMulti, i, times, data, initConds, initPar
 		}, error = function(e) {
 			print("optim failed")
 		})
-
-		predInfectiousAll <- evalSIRMulti(times, data, initConds, optimParams, tsExplore, k)
-		predInfectiousPast <- predInfectiousAll[1:i]
-		predInfectiousFuture <- predInfectiousAll[(i+1):length(predInfectiousAll)]
-		if ((i+8) < length(predInfectiousAll)) {
-			predInfectiousFuture1 <- predInfectiousAll[(i+1):(i+8)]
-			futureData1 <- data[(i+1):(i+8)]
-		} else {
-			futureData1 <-  data[(i+1):length(data)]
-		}
-		# if (i==8) {
-		# 	print(futureData1)
-		# 	print(predInfectiousFuture1)
-		# }
-		# TODO: Don't calc all Rsq inside parallel section
-		futureData <- data[(i+1):length(data)]
-		rSquareErrorAll <- rSquareError(predInfectiousAll, data)
+		predInfectiousPast <- evalSIRMulti(truncTimes, truncData, initConds, optimParams, tsExplore, k)
+		# rSquare error to determine best time to start fitting
 		rSquareErrorPast <- rSquareError(predInfectiousPast, truncData)
-		rSquareErrorFuture <- rSquareError(predInfectiousFuture, futureData)
-		rSquareErrorFuture1 <- rSquareError(predInfectiousFuture, futureData1)
-		finalRes <- (data[i] - predInfectiousAll[i])
-		c(rSquareErrorFuture, rSquareErrorFuture1, rSquareErrorAll, finalRes, t, rSquareErrorPast, optimParams)
+		# Build list of optimisation results
+		c(t, rSquareErrorPast, optimParams)
 	}
-	# t1 <- proc.time()
-	# print(t1-t0)
-	# readline()
+
 	# Find max rSqaureError entry
 	if (length(range) > 1) {
-		optimIndex <- which.max(allOptimParams[,6])
-		optimParams <- allOptimParams[optimIndex,]
+		# Find index with best fit according to max RSq
+		optimIndex <- which.max(allOptimParams[,2])
+		# Get max parameters
+		optimList <- allOptimParams[optimIndex,]
 	} else {
-		optimParams <- allOptimParams
+		# Base case when vector of single results
+		optimList <- allOptimParams
 	}
-	rSquareFuture <- optimParams[1]
-	rSquareFuture1 <- optimParams[2]
-	rSquareAll <- optimParams[3]
-	finalRes <- optimParams[4]
-	rSquarePast <- optimParams[6]
-	optimParams <- optimParams[5:length(optimParams)]
+
+	optimTime <- optimList[1]
+	optimRSquare <- optimList[2]
+	# Index optim params to length of list to get all params
+	optimParams <- optimList[3:length(optimList)]
 
 	# EvaluateSIR over to plot fitted epidemics
 	par(mar=c(7.1,4.1,4.1,2.1))
@@ -81,22 +61,14 @@ fitInRangeParallel <- function(optimSIRMulti, i, times, data, initConds, initPar
 	title(main=plotConfig$title, cex.main=1, cex.axis=0.8)
 	daysText <- paste("Epochs after outbreak = ", i)
 	mtext(daysText, 3, cex=0.8)
-	RSqPastText <- paste("Past RSquare = ", signif(rSquarePast, digits=3))
-	RSqFutureText <- paste("Future RSquare = ", signif(rSquareFuture, digits=3))
-	# RSqFuture1Text <- paste("Future T7 RSquare = ", signif(rSquareFuture1, digits=3))
-	RSqAllText <- paste("All RSquare = ", signif(rSquareAll, digits=3))
-	# mtext(RSqText, 1, at=10, padj=6, cex=0.5)
+	RSqPastText <- paste("Past RSquare = ", signif(optimRSquare, digits=3))
 	mtext(RSqPastText, 1, at=plotConfig$rat, padj=6, cex=0.7) #other
-	mtext(RSqFutureText, 1, at=plotConfig$rat, padj=8, cex=0.7) #other
-	# mtext(RSqFuture1Text, 1, at=plotConfig$rat, padj=10, cex=0.7) #other
-	mtext(RSqAllText, 1, at=plotConfig$rat, padj=10, cex=0.7) #other
-	# mtext(RSqText, 1, at=30, padj=6, cex=0.5) #bl
-	# mtext(RSqText, 1, at=20, padj=6, cex=0.5) #hs
 	lines(1:length(times), data, col='steelblue')
 	points(1:length(truncTimes), truncData, col='black', pch=16)
-	allPredInf <- decomposeEpidemics(times, data, initConds, optimParams[3:length(optimParams)], c(ts[1:k-1],optimParams[1]), k, actualFit, plotConfig)
+
+	allPredInf <- decomposeEpidemics(times, data, initConds, optimParams, c(ts[1:k-1],optimTime), k, actualFit, plotConfig)
+	allPredInf$optimTime <- optimTime
 	allPredInf$optimParams <- optimParams
-	allPredInf$rSquares <- c(rSquareFuture, rSquareFuture1, rSquareAll, rSquarePast)
-	allPredInf$finalRes <- finalRes;
+	allPredInf$optimRSquare <- optimRSquare
 	allPredInf
 }
--- core/fitOverTimeMulti.r
+++ core/fitOverTimeMulti.r
@@ -33,7 +33,7 @@ fitOverTimeMulti <- function(optimMethod, times, data, initConds, initParams, of
 	ts <- c(1)
 	# Set the number of epidemics
 	k <- 1
-	allParams <- c()
+	allPredInfList <- c()
 	# Number of increasing residuals
 	nRes <- 3
 	nIncRes <- c()
@@ -51,19 +51,19 @@ fitOverTimeMulti <- function(optimMethod, times, data, initConds, initParams, of
 		print("### Fit k", quote=FALSE); print(paste(c("fitting "," of "), c(i, maxTruncation)), quote=FALSE)
 		allPredInf <- fitInRangeParallel(setSolver(optimMethod, k), i, offsetTimes, offsetData, initConds, initParams, ts, k, c(minTRange:(i-maxTRange)), 1, plotConfig)
 		# Store allPredInf
-		allParams[[i]] <- allPredInf
-		maxVals <- allPredInf$optimParams
-		nIncRes[i] <- allPredInf$finalRes
-		lastNRes <- nIncRes[(i-(nRes-1)):i]
-		maxt <- maxVals[1]
-		rSquare <- maxVals[2]
+		allPredInfList[[i]] <- allPredInf
+
+		maxt <- allPredInf$optimTime
 		ts[k] <- maxt
+		rSquare <- allPredInf$optimRSquare
+		optimParams <- allPredInf$optimParams
 
 
 		# Try to improve fit if rSquare has deteriorated
 		lim <- thresholds$lim
 		diff <- thresholds$diff
-		if (rSquare < lim && incMag(lastNRes, nRes)) {
+		# if (rSquare < lim && incMag(lastNRes, nRes)) {
+		if (rSquare < lim) {
 			# Try k+1 epidemics
 			print(">>> Fit k+1", quote=FALSE)
 			# Set initial parameters
@@ -71,32 +71,29 @@ fitOverTimeMulti <- function(optimMethod, times, data, initConds, initParams, of
 			initCondsMulti <- c(initConds, startConds)
 			# Fit k+1 epidemics
 			allPredInf <- fitInRangeParallel(setSolver(optimMethod, k+1), i, offsetTimes, offsetData, initCondsMulti, initParamsMulti, ts, k+1, c(minTRange:(i-maxTRange)), 2, plotConfig)
-			maxValsMulti <- allPredInf$optimParams
-			nIncRes[i] <- allPredInf$finalRes
-			lastNRes <- nIncRes[(i-(nRes-1)):i]
-			multiRSquare <- maxValsMulti[2]
-			print("multi rs")
-			print(multiRSquare)
-			print("rs")
-			print(rSquare)
+			# nIncRes[i] <- allPredInf$finalRes
+			# lastNRes <- nIncRes[(i-(nRes-1)):i]
+			multiRSquare <- allPredInf$optimRSquare
+
 			# If k+1 is significantly better then continue with k+1 fit
 			# if ((multiRSquare - rSquare) > diff || incMag(lastNRes, nRes)) {
+			if ((multiRSquare - rSquare) > diff) {
 				print("!!!Set k+1", quote=FALSE)
 				# Set k+1 epidemics from now on
 				k <- k + 1
 				# Update parameters to continue fitting with k+1 epidemics
-				maxVals <- maxValsMulti
-				maxt <- maxVals[1]
+				optimParams <- allPredInf$optimParams
+				maxt <- allPredInf$optimTime
 				ts <- c(ts,maxt)
-				rSquare <- maxVals[2]
+				rSquare <- multiRSquare
 				initConds <- initCondsMulti
-			# }
+			}
 		}
 		# Update the initial parameters for the next fitting
-		initParams <- maxVals[3:length(maxVals)]
+		initParams <- optimParams
 	}
 	# Save all params
-	save(allParams, file=plotConfig$dataFile)
+	save(allPredInfList, file=plotConfig$dataFile)
 	save.image(plotConfig$envFile)
 }
 
