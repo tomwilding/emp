5d3946928ffd46f2396c8abf4a1e5b4cd87196d4
tomwilding <tomwilding72@gmail.com>
2014-02-17 00:16:41 +0000

    Remove assertive

--- setPackages.r
+++ setPackages.r
@@ -4,6 +4,6 @@ setPackages <- function() {
 	install.packages("doMC")
 	install.packages("foreach")
 	# install.packages("emdbook", lib="Rpackages")
-	install.packages("GillespieSSA", lib="Rpackages")
-	install.packages("assertive")
+	# install.packages("GillespieSSA", lib="Rpackages")
+	# install.packages("assertive")
 }
