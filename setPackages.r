9e74b4900ce0de261d12980958536a0ec8ba021a
tomwilding <tomwilding72@gmail.com>
2014-02-17 00:06:24 +0000

    Added setPackaes

---
+++ setPackages.r
@@ -0,0 +1,8 @@
+setPackages <- function() {
+	# install.packages("bbmle", lib="Rpackages")
+	install.packages("deSolve")
+	install.packages("doMC")
+	install.packages("foreach")
+	# install.packages("emdbook", lib="Rpackages")
+	install.packages("GillespieSSA", lib="Rpackages")
+}
