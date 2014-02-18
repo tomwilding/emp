241b82a9a814c3a76812ab56f6cea5abe962acd3
tomwilding <tomwilding72@gmail.com>
2014-02-18 13:26:30 +0000

    Jacks amazing bash script

--- launch/runFitting.sh
+++ launch/runFitting.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-ssh pixel01 << EOF
+i=1
+for arg in "$@"; do
+	i=i+1
+	ssh pixel0"$i" << EOF
         cd emp
         R CMD BATCH sources.r
-        R CMD BATCH launch/processBlur.r
-EOF
+        R CMD BATCH launch/"$arg".r
+	EOF
+done
+
