64498062521a9e5698179f899e7b81fa4ae4a1c8
tomwilding <tomwilding72@gmail.com>
2014-02-18 16:10:06 +0000

    echo bash cmd

--- launch/runFitting.sh
+++ launch/runFitting.sh
@@ -2,5 +2,6 @@
 i=0
 for arg in "$@"; do
         let "i+=1"
+        echo ssh pixel0"$i" 'cd emp;R CMD BATCH sources.r;R CMD BATCH launch/'"$arg"'.r'
         ssh pixel0"$i" 'cd emp;R CMD BATCH sources.r;R CMD BATCH launch/'"$arg"'.r' &
 done
