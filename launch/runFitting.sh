784408fa7a263f1005562af4295ea963f37e5734
tomwilding <tomwilding72@gmail.com>
2014-02-18 16:55:09 +0000

    Changed out file

--- launch/runFitting.sh
+++ launch/runFitting.sh
@@ -2,5 +2,5 @@
 i=0
 for arg in "$@"; do
         let "i+=1"
-        ssh pixel0"$i" 'cd emp;R CMD BATCH launch/'"$arg"'.r' &
+        ssh pixel0"$i" 'cd emp;R CMD BATCH -q launch/'"$arg"'.r output/'"$arg"'.txt' &
 done
