a720dfc331f30acdde28500b64ed911d5621ec5f
tomwilding <tomwilding72@gmail.com>
2014-02-18 16:59:24 +0000

    Added if case to check args

--- launch/runFitting.sh
+++ launch/runFitting.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 i=0
+if ["$#" = 0]; then
+	echo Not Enough Args
+fi
 for arg in "$@"; do
         let "i+=1"
         ssh pixel0"$i" 'cd emp;R CMD BATCH -q launch/'"$arg"'.r output/'"$arg"'.txt' &
