c44e0b0bc7921ad8c47a2e02092e570d765dfae1
tomwilding <tomwilding72@gmail.com>
2014-02-27 20:55:06 +0000

    Removed plotting

--- core/fitInRangeParallel.r
+++ core/fitInRangeParallel.r
@@ -74,36 +74,6 @@ fitInRangeParallel <- function(optimSIRMulti, i, offsetTimes, offsetData, initCo
 	startOffset <- offsets$startOffset
 	endOffset <- offsets$endOffset
 	
-
-	fineTimes <- breakTime(offsetTimes, timeStep)
-	cl <- c("red","cyan","forestgreen","goldenrod2","red4")
-	setEPS()
-	graphName <- paste("t", i, sep='')
-	graphName <- paste(graphName, ".eps", sep='')
-	postscript(paste(plotConfig$fileName, graphName, sep=''))	
-	par(mar=c(7.1,4.1,4.1,2.1))
-	plot(1:length(offsetTimes), offsetData, xlab='Epochs', ylab='Infected Individuals', col='steelblue')
-	title(main=plotConfig$title, cex.main=1, cex.axis=0.8)
-	daysText <- paste("Epochs after outbreak = ", i)
-	mtext(daysText, 3, cex=0.8)
-	
-	# Plot offsetData points and actual offsetData lines
-	lines(offsetTimes, offsetData, col='steelblue', lty=1)
-	points(truncTimes, truncData, col='black', pch=16)
-
-	# lines(fineTimes, allEvalFine$multiInf, lty=1)
-	multiInf <- allEvalFine$multiInf
-	for(k in 1:(length(allEvalFine$subInf))) {
-		sub <- allEvalFine$subInf[[k]]
-		subParams <- allEvalFine$subParams[[k]]
-		# Print sub epidemic graph
-		lines(fineTimes, sub, col=cl[k], lty=2)
-		lines(fineTimes, multiInf, col="black")
-	}
-	dev.off()
-
-	# eval <- decomposeEpidemics(offsetTimes, offsetData, initConds, optimParams, c(ts[1:k-1], optimTime), k, actualFit, plotConfig)
-	
 	# Set values of eval
 	eval$multiParams <- optimParams
 	eval$initConds <- initConds
