50f08cc07c92b55e9ffc00b2822184855b2390e9
tomwilding <tomwilding72@gmail.com>
2014-03-12 11:42:48 +0000

    Taken out graph

--- core/fitInRangeParallel.r
+++ core/fitInRangeParallel.r
@@ -76,32 +76,32 @@ fitInRangeParallel <- function(optimSIRMulti, i, offsetTimes, offsetData, initCo
 	endOffset <- offsets$endOffset
 
 
-	# Plot inline for dev
- 	fineTimes <- breakTime(offsetTimes, timeStep)
- 	cl <- c("red","cyan","forestgreen","goldenrod2","red4")
- 	setEPS()
- 	r <- plotConfig$run
- 	graphName <- paste("t", i, sep='')
- 	graphName <- paste(graphName, ".eps", sep='')
- 	postscript(paste(plotConfig$fileName, graphName, sep=''))	
- 	par(mar=c(7.1,4.1,4.1,2.1))
- 	plot(1:length(offsetTimes), offsetData, xlab='Epochs', ylab='Infected Individuals', col='steelblue')
- 	title(main=plotConfig$title, cex.main=1, cex.axis=0.8)
- 	daysText <- paste("Epochs after outbreak = ", i)
- 	mtext(daysText, 3, cex=0.8)
- 	# Plot offsetData points and actual offsetData lines
- 	lines(offsetTimes, offsetData, col='steelblue', lty=1)
- 	points(truncTimes, truncData, col='black', pch=16)
- 	# lines(fineTimes, allEvalFine$multiInf, lty=1)
- 	multiInf <- allEvalFine$multiInf
- 	for(k in 1:(length(allEvalFine$subInf))) {
- 		sub <- allEvalFine$subInf[[k]]
- 		subParams <- allEvalFine$subParams[[k]]
- 		# Print sub epidemic graph
- 		lines(fineTimes, sub, col=cl[k], lty=2)
- 		lines(fineTimes, multiInf, col="black")
- 	}
- 	dev.off()
+	# # Plot inline for dev
+ # 	fineTimes <- breakTime(offsetTimes, timeStep)
+ # 	cl <- c("red","cyan","forestgreen","goldenrod2","red4")
+ # 	setEPS()
+ # 	r <- plotConfig$run
+ # 	graphName <- paste("t", i, sep='')
+ # 	graphName <- paste(graphName, ".eps", sep='')
+ # 	postscript(paste(plotConfig$fileName, graphName, sep=''))	
+ # 	par(mar=c(7.1,4.1,4.1,2.1))
+ # 	plot(1:length(offsetTimes), offsetData, xlab='Epochs', ylab='Infected Individuals', col='steelblue')
+ # 	title(main=plotConfig$title, cex.main=1, cex.axis=0.8)
+ # 	daysText <- paste("Epochs after outbreak = ", i)
+ # 	mtext(daysText, 3, cex=0.8)
+ # 	# Plot offsetData points and actual offsetData lines
+ # 	lines(offsetTimes, offsetData, col='steelblue', lty=1)
+ # 	points(truncTimes, truncData, col='black', pch=16)
+ # 	# lines(fineTimes, allEvalFine$multiInf, lty=1)
+ # 	multiInf <- allEvalFine$multiInf
+ # 	for(k in 1:(length(allEvalFine$subInf))) {
+ # 		sub <- allEvalFine$subInf[[k]]
+ # 		subParams <- allEvalFine$subParams[[k]]
+ # 		# Print sub epidemic graph
+ # 		lines(fineTimes, sub, col=cl[k], lty=2)
+ # 		lines(fineTimes, multiInf, col="black")
+ # 	}
+ # 	dev.off()
 
 	# Set values of eval
 	eval$multiParams <- optimParams
