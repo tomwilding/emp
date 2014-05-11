550d37f4d6b2e60a5458bded07dad7b481765ed3
tomwilding <tomwilding72@gmail.com>
2014-05-11 21:37:41 +0100

    CDC single

--- launch/processCDC.r
+++ launch/processCDC.r
@@ -7,26 +7,23 @@ fluData <- read.csv("data/FluView_Data.csv", header = FALSE)
 percentPositive <- fluData[,11] / 100
 totalSpecimins <- fluData[,3]
 data <- totalSpecimins * percentPositive
-data <- c(numeric(30), data)
+# data <- c(numeric(30), data)
 # Fitting epidemics
-startOffset <- 4
+startOffset <- 1
 endOffset <- 1
-minTruncation <- 1
+minTruncation <- 4
 offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=minTruncation)
 # Thresholds
 thresholds <- list(diff=0.05, lim=0.9)
 plotConfig <- list(title="Synthedemic Decomposition of CDC Data", fileName="output/graphs/CDC/", dataFile="output/data/CDC/CDCData.RData", envFile="output/data/CDC/CDCEnv.RData", pat=5, rat=30)
 
 # Init Params = beta, gamma, S0
-# Maybe update in iteration to start at the previous optim params in the next iteration?
-# Assume at first point there are significantly more Susceptible than Infected?
-initParams <- c();
-
-epiTypes <- c(0)
-
+initParams <- c(log(0.001), log(0.1), log(orderOf(data[1])*10))
+# Epidemic type array epidemic types correspond to the number of parameters of the sub epidemic model
+epiTypes <- c(3)
 # Init Conds = S0, I0, R0
 # I0 from first data point
-initConds <- c();
+initConds <- c(1, orderOf(data[1]), 0)
 
 # Fit parameters at different times
 fitOverTimeMulti("LMS", c(1:length(data)), data, initConds, initParams, epiTypes, offsets, thresholds, plotConfig)
