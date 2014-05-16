7cb91d7c47811ac5a0182ab9219672b9cd54eade
tomwilding <tomwilding72@gmail.com>
2014-05-16 02:17:24 +0100

    Blur OO 0.1

--- core/evalMulti.r
+++ core/evalMulti.r
@@ -23,7 +23,7 @@ evalMulti <- function(times, data, initConds, params, epiTypes, ts, k, granulari
 			if (subEpiNumParams == 3) {
 				# Update SIR epidemic parameters
 				# Update S0
-				initCondsMulti[1] <- exp(paramsMulti[3])
+				# initCondsMulti[1] <- exp(paramsMulti[3])
 				# print(initCondsMulti)
 				# Update I0 computed using previous sub epidemics
 				initCondsMulti[2] <- I0
