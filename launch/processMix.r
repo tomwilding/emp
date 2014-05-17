cf9f2774fd4768b80963949dbed3f102cb743790
tomwilding <tomwilding72@gmail.com>
2014-05-17 01:05:11 +0100

    mix from start

--- launch/processMix.r
+++ launch/processMix.r
@@ -1,7 +1,6 @@
 # require('epi')
 # Simulate data 
 fluData <- simSIR(0.001,0.05,400,1)
-# fluData1 <- simExp(0.2,200)
 fluData1 <- simSIR(0.001,0.1,400,1)
 # Get data from dataframe
 # Ensure first is larger than second
@@ -50,15 +49,15 @@ offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=minT
 thresholds <- list(lim=0.998)
 
 # Init Params = beta, gamma, S0
-# initParams <- c();
-initParams <- c(log(0.001), log(0.01), log(10));
+initParams <- c();
+# initParams <- c(log(0.001), log(0.01), log(10));
 # Epidemic type array epidemic types correspond to the number of parameters of the sub epidemic model
-# epiTypes <- c(0)
-epiTypes <- c(0, 3)
+epiTypes <- c(0)
+# epiTypes <- c(0, 3)
 # Init Conds = S0, I0, R0
 # I0 from first data point
-# initConds <- c();
-initConds <- c(1,1,0)
+initConds <- c();
+# initConds <- c(1,1,0)
 
 plotConfig <- list(title="Synthedemic Decomposition of Simulated Data", fileName="output/graphs/mix/", dataFile="output/data/mix/mixData.RData", envFile="output/data/mix/mixEnv.RData", pat=5, rat=30)
 # Fit parameters
