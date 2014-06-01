a796df54026aa480bfd59671779ac9a539cc2cd4
tomwilding <tomwilding72@gmail.com>
2014-06-01 01:22:48 +0100

    mix from start

--- launch/processMix.r
+++ launch/processMix.r
@@ -49,17 +49,17 @@ offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=minT
 thresholds <- list(lim=0.995)
 
 # Init Params = beta, gamma, S0
-# initParams <- c()
-initParams <- c(log(0.001), log(0.01), log(1000), logit((34 - 20), (34 - minTruncation), 34),
-				log(0.001), log(0.01), log(1000), logit((85 - 20), (85 - minTruncation), 85))
+initParams <- c()
+# initParams <- c(log(0.001), log(0.01), log(1000), logit((34 - 20), (34 - minTruncation), 34),
+# 				log(0.001), log(0.01), log(1000), logit((85 - 20), (85 - minTruncation), 85))
 # Epidemic type array epidemic types correspond to the number of parameters of the sub epidemic model
-# epiTypes <- c(0)
-epiTypes <- c(0, 4, 4)
+epiTypes <- c(0)
+# epiTypes <- c(0, 4, 4)
 
 # Init Conds = S0, I0, R0
 # I0 from first data point
-# initConds <- c()
-initConds <- c(1,1,0,0, 1,1,0,0)
+initConds <- c()
+# initConds <- c(1,1,0,0, 1,1,0,0)
 
 plotConfig <- list(title="Synthedemic Decomposition of Simulated Data", fileName="output/graphs/mixOT/", dataFile="output/data/mix/mixDataFT.RData", envFile="output/data/mix/mixEnv.RData", pat=5, rat=30)
 
