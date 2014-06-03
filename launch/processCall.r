39b46034463e7dc3d0efa650d853d6077e7b91ba
tomwilding <tomwilding72@gmail.com>
2014-06-03 15:35:04 +0100

    Offset and Call

--- launch/processCall.r
+++ launch/processCall.r
@@ -9,7 +9,7 @@ data <- fluData[,2]
 # Fitting epidemics
 startOffset <- 1
 endOffset <- 1
-offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=5)
+offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=4)
 
 # Threshold
 thresholds <- list(lim=0.9)
@@ -17,12 +17,12 @@ thresholds <- list(lim=0.9)
 # Init Params = beta, gamma, S0
 initParams <- c()
 # Epidemic type array epidemic types correspond to the number of parameters of the sub epidemic model
-epiTypes <- c()
+epiTypes <- c(0)
 # Init Conds = S0, I0, R0
 # I0 from first data point
 initConds <- c()
 
-plotConfig <- list(title="Synthedemic Decomposition of Caly Rae Jepsen BitTorrent Downloads", fileName="output/graphs/call1/", dataFile="output/data/call1/callData.RData", envFile="output/data/call/callEnv.RData", pat=12, rat=60)
+plotConfig <- list(title="Synthedemic Decomposition of Carly Rae Jepsen BitTorrent Downloads", fileName="output/graphs/call2/", dataFile="output/data/call/callData.RData", envFile="output/data/call/callEnv.RData", pat=12, rat=60)
 
 # Fit parameters
 fitOverTimeMulti("LMS", 1:length(data), data, initConds, initParams, epiTypes, offsets, thresholds, plotConfig)
---
+++ plot/plotOffset.r
@@ -0,0 +1,60 @@
+load("output/data/offset/offsetData.RData")
+require('epi')
+# Simulate data 
+# require('epi')
+
+fluData1 <- simSIR(0.002,0.1,400,1)
+# fluData1 <- simExp(0.2,400)
+# fluData2 <- simSIR(0.002,0.1,500,10)
+# Get data from dataframe
+# Ensure first is larger than second
+# nSum <- 4
+positiveInfectious1 <- fluData1$data[,3]
+# positiveInfectious <- sumData(fluData$data[,3], nSum)
+# positiveInfectious2 <- fluData2$data[,3]
+# positiveInfectious1 <- sumData(fluData1$data[,3], nSum)
+
+# Offset of t0 for second epidemic
+# offset1 <- 50
+offset1 <- 50
+# offset2 <- 100
+# Total length of the combined data assuming last epidemic is longest time
+totalLength <- offset1 + length(positiveInfectious1)
+# Padding of zeros to offset data
+positiveInfectiousPadStart1 <- array(0, offset1)
+# Combine data with padding offset zeros
+# Add together the different predicted infectious values truncated to required size
+data <- c(positiveInfectiousPadStart1, positiveInfectious1)
+# data <- positiveInfectious1
+tmax <- length(data)
+times <- c(1:tmax)
+
+# Fitting epidemics
+startOffset <- 1
+endOffset <- 1
+minTruncation <- 4
+offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=minTruncation)
+
+# Target rSquare error
+thresholds <- list(lim=0.9)
+
+# Epidemic type array epidemic types correspond to the number of parameters of the sub epidemic model
+epiTypes <- c(0)
+# epiTypes <- c(4)
+
+initParams <- c()
+# initParams <- c(log(0.001), log(0.1), log(10), 0)
+# Init Conds = S0, I0, R0
+# I0 from first data point
+initConds <- c()
+# initConds <- c(1,log(data[startOffset + 1]),0,0)
+
+plotConfig <- list(title="Synthedemic Decomposition of Simulated Data", fileName="output/graphs/offset/", dataFile="output/data/offset/offsetData.RData", envFile="output/data/offset/offsetEnv.RData", pat=10, rat=80)
+# Fit parameters
+reconstructPlot(times, data, offsets, thresholds, initParams, initConds, plotConfig)
+
+# Plot Residuals
+# plotResiduals(times, data, offsets, thresholds, initParams, initConds, plotConfig)
+
+# t+1 prediction
+# plotPred(times, data, offsets, thresholds, initParams, initConds, plotConfig)
