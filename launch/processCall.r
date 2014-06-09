7e8d6d9f53feb4a88e597c9b697a0dfe68916643
tomwilding <tomwilding72@gmail.com>
2014-06-09 13:31:01 +0100

    changed fp call295

--- launch/processCall.r
+++ launch/processCall.r
@@ -21,7 +21,7 @@ epiTypes <- c(0)
 # Init Conds = S0, I0, R0
 # I0 from first data point
 initConds <- c()
-plotConfig <- list(title="Synthedemic Decomposition of Carly Rae Jepsen BitTorrent Downloads", fileName="output/graphs/callFinal2/", dataFile="output/data/call/callFinal3.RData", envFile="output/data/call/callEnv.RData", pat=12, rat=60)
+plotConfig <- list(title="Synthedemic Decomposition of Carly Rae Jepsen BitTorrent Downloads", fileName="output/graphs/callFinal2/", dataFile="output/data/call/callFinal295.RData", envFile="output/data/call/callEnv.RData", pat=12, rat=60)
 
 # Fit parameters
 fitOverTimeMulti("LMS", 1:length(data), data, initConds, initParams, epiTypes, offsets, thresholds, plotConfig)
