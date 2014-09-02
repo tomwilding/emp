9a943f2d9eefb1db9e6221123261bbdc0651417a
tomwilding <tomwilding72@gmail.com>
2014-09-02 22:47:42 +0100

    removed graphs no lim

--- launch/processBlur.r
+++ launch/processBlur.r
@@ -7,7 +7,7 @@ data <- epiData[,2]
 # data <- sumData(data, nSum)
 
 # Fitting epidemics
-minTruncation <- 4
+minTruncation <- 1
 # startOffset <- findStartOffset(data, minTruncation)
 startOffset <- 1
 endOffset <- 1
@@ -17,15 +17,15 @@ offsets <- list(startOffset=startOffset, endOffset=endOffset, minTruncation=minT
 thresholds <- list(lim=0.9)
 
 # Init Params = beta, gamma, S0
-initParams <- c(log(0.001), log(0.01), log(1000));
+initParams <- c(log(1));
 
-epiTypes <- c(3)
+epiTypes <- c(1)
 
 # Init Conds = S0, I0, R0
 # I0 from first data point
-initConds <- c(1,data[startOffset],0);
+initConds <- c(1);
 
-plotConfig <- list(title="Synthedemic Decomposition of Robin Thicke BitTorrent Downloads", fileName="output/graphs/blur/", dataFile="output/data/blurSingle/blurDataStartSIR2.RData", envFile="output/data/blur2/blurEnv.RData", pat=12, rat=60)
+plotConfig <- list(title="Synthedemic Decomposition of Robin Thicke BitTorrent Downloads", fileName="output/graphs/blur/", dataFile="output/data/blur/blurDataBaselineNoLims.RData", envFile="output/data/blur2/blurEnv.RData", pat=12, rat=60)
 
 # Fit parameters
 fitOverTimeMulti("LMS", c(1:length(data)), data, initConds, initParams, epiTypes, offsets, thresholds, plotConfig)
